a013a7f87eebd80ad02cd219a8a4024ab0552c5c
Emir <emir.emiroguz@gmail.com>
2021-11-22 13:18:00 +0100

    security_mode, device_id & battery_status added

--- drivers/sensor/bq35100/bq35100.h
+++ drivers/sensor/bq35100/bq35100.h
@@ -309,6 +309,9 @@ struct bq35100_config {
 	uint16_t design_capacity;
 	int16_t cal_voltage;
 	bq35100_gauge_mode_t gauge_mode;
+	bq35100_security_mode_t security_mode;
+	uint8_t device_id;
+	uint8_t battery_status;
 };
 
 #endif  /* ZEPHYR_DRIVERS_SENSOR_BQ35100_BQ35100_H_ */
